9234d60a45d943a5817477871d62495077d5bc81
Pascal Gagneur <pascal.gagneur@postkodlotteriet.se>
2013-11-17 15:07:46 +0100

    Added conseil image text and style

--- conseils.html.php
+++ conseils.html.php
@@ -1,11 +1,25 @@
 <h1><span>Conseils</span></h1>
 <article>
 
-        <img class="product-image" src="img/memorable.png" alt="memorable" width="890" height="490" />
-        <p class="product-text">
-            Voici Le Mémorable, notre gâteau aux fruits au sirop d'érable. <br>Il est rempli de fruits et de noix, et affiné au whisky pendant 30 jours. <br>
-            Pour profiter pleinement de sa saveur, conservez-le au frais et servez-le <br>à la température ambiante. C'est un pur délice québécois!<br>
-            <br>Disponible en grand et petit format.
+        <img class="product-image" src="img/conseil_01.jpg" alt="" width="890" height="567" />
+        <p class="conseil-text">
+            L'endroit idéal pour conserver votre gâteau est dans un endroit frais et sec.
+            Afin de faciliter la coupe, nous conseillons de réfrigérer votre gâteau pendant environ 5 heures. En le coupant froid, vous éviterez de l’émietter.
         </p>
+        <img class="product-image" src="img/conseil_02.jpg" alt="" width="890" height="567" />
+        <p class="conseil-text">
+            À l’aide d’un couteau à lame droite bien aiguisé, couper de mince tranches d’environ 5 mm.
+        </p>
+    <img class="product-image" src="img/conseil_03.jpg" alt="" width="890" height="567" />
+    <p class="conseil-text">
+        Servir tempéré
+        <br/>
+        En dessert accompagné de cidre de glace ou de porto
+        Dans un plateau de fromages fins ( cheddars fort, tommes, etc.) accompagné de figues fraîches, suprêmes d’orange.
+        En biscotte, pour accompagner le foie gras.
+        <br/><br/>
+        Pour obtenir de belles biscottes :
+        Congeler le gâteau puis à l’aide d’un couteau très bien aiguisé, trancher-le le plus finement possible, enfourner les tranches à 325 F° environ 15 minutes. Laisser refroidir les tranches avant de les utiliser.
+    </p>
 
 </article>
