c83617a4f8b47449ad89f60185c3d776b03ac287
Pascal Gagneur <pascal.gagneur@postkodlotteriet.se>
2013-11-20 00:55:46 +0100

    Added a new prod

--- products.html.php
+++ products.html.php
@@ -1,46 +1,55 @@
 <h1><span>Produits</span></h1>
-         <article id="prod5">
-        	<h2>Product name</h2>
-        	<div class="product-info">
-         		<p class="product-text">
-        		Voici Le Mémorable, notre gâteau aux fruits au sirop d'érable. <br>Il est rempli de fruits et de noix, et affiné au whisky pendant 30 jours. <br>
-                Pour profiter pleinement de sa saveur, conservez-le au frais et servez-le <br>à la température ambiante. C'est un pur délice québécois!<br>
-                 <br>Disponible en grand et petit format.
-        		</p>
-     	      <img class="product-image" src="img/memorable.png" alt="memorable" width="890" height="490" />
-        	</div>
-        </article>
-        <article id="prod1">
-        	<h2>Product name</h2>
-          <div class="product-info">
-            <p class="product-text">
-        		Voici Le Distingué, notre gâteau aux fruits classique. <br>Il est rempli de fruits et de noix, et affiné au rhum pendant 30 jours. <br>Pour profiter pleinement de sa saveur, conservez-le au frais et servez-le <br>à la température ambiante. Il honorera les meilleurs portos!<br>
-                 <br>Disponible en grand et petit format. 
-         		</p>
-         		<img class="product-image" src="img/distingue1.jpg" alt="distingue1" width="890" height="490" />
-            <img class="product-image" src="img/distingue2.jpg" alt="distingue2" width="890" height="490" />
-            <img class="product-image" src="img/distingue3.jpg" alt="distingue3" width="890" height="490" />
-        	</div>
-        </article>
-           <article id="prod6">
-        	<h2>Product name</h2>
-        	<div class="product-info">
-         		<p class="product-text">
-        		Voici L'Audacieux, notre gâteau aux fruits au chocolat noir à 70% de cacao. <br>Il est rempli de fruits, de noix, et affiné à la bière noire pendant 30 jours. <br>
-                Pour profiter pleinement de sa saveur, conservez-le au frais et servez-le <br>à la température ambiante. Régalez-vous!<br>
-                 <br>Disponible en grand et petit format.
-        		</p>
-     	      <img class="product-image" src="img/audacieux.png" alt="audacieux" width="890" height="490" />
-        	</div>
-        </article>
-           <article id="prod4">
-        	<h2>Product name</h2>
-        	<div class="product-info">
-         		<p class="product-text">
-        		Voici Le Distingué - Édition un an, notre gâteau aux fruits classique <br>que nous avons soigneusement affiné au rhum pendant un an. <br>Il est exceptionnellement parfumé et combien savoureux! <br>Conservez-le au frais et servez-le à la température ambiante, <br>accompagné d'un porto ou d'un cidre de glace.<br>
-                 <br>Disponible en grand format.
-        		</p>
-     	      <img class="product-image" src="img/distingueunan1.jpg" alt="distingueunan1" width="890" height="490" />
-        	</div>
-        </article>
- 
+<article id="prod5">
+    <h2>Product name</h2>
+    <div class="product-info">
+        <p class="product-text">
+        Voici Le Mémorable, notre gâteau aux fruits au sirop d'érable. <br>Il est rempli de fruits et de noix, et affiné au whisky pendant 30 jours. <br>
+        Pour profiter pleinement de sa saveur, conservez-le au frais et servez-le <br>à la température ambiante. C'est un pur délice québécois!<br>
+         <br>Disponible en grand et petit format.
+        </p>
+      <img class="product-image" src="img/memorable.png" alt="memorable" width="890" height="490" />
+    </div>
+</article>
+<article id="prod1">
+    <h2>Product name</h2>
+    <div class="product-info">
+    <p class="product-text">
+        Voici Le Distingué, notre gâteau aux fruits classique. <br>Il est rempli de fruits et de noix, et affiné au rhum pendant 30 jours. <br>Pour profiter pleinement de sa saveur, conservez-le au frais et servez-le <br>à la température ambiante. Il honorera les meilleurs portos!<br>
+         <br>Disponible en grand et petit format.
+        </p>
+        <img class="product-image" src="img/distingue1.jpg" alt="distingue1" width="890" height="490" />
+    <img class="product-image" src="img/distingue2.jpg" alt="distingue2" width="890" height="490" />
+    <img class="product-image" src="img/distingue3.jpg" alt="distingue3" width="890" height="490" />
+    </div>
+</article>
+<article id="prod6">
+    <h2>Product name</h2>
+    <div class="product-info">
+        <p class="product-text">
+        Voici L'Audacieux, notre gâteau aux fruits au chocolat noir à 70% de cacao. <br>Il est rempli de fruits, de noix, et affiné à la bière noire pendant 30 jours. <br>
+        Pour profiter pleinement de sa saveur, conservez-le au frais et servez-le <br>à la température ambiante. Régalez-vous!<br>
+         <br>Disponible en grand et petit format.
+        </p>
+      <img class="product-image" src="img/audacieux.png" alt="audacieux" width="890" height="490" />
+    </div>
+</article>
+<article id="prod4">
+    <h2>Product name</h2>
+    <div class="product-info">
+        <p class="product-text">
+        Voici Le Distingué - Édition un an, notre gâteau aux fruits classique <br>que nous avons soigneusement affiné au rhum pendant un an. <br>Il est exceptionnellement parfumé et combien savoureux! <br>Conservez-le au frais et servez-le à la température ambiante, <br>accompagné d'un porto ou d'un cidre de glace.<br>
+         <br>Disponible en grand format.
+        </p>
+      <img class="product-image" src="img/distingueunan1.jpg" alt="distingueunan1" width="890" height="490" />
+    </div>
+</article>
+<article id="prod2">
+    <h2>Product name</h2>
+    <div class="product-info">
+        <p class="product-text">
+            Voici Le Distingué - Édition un an, notre gâteau aux fruits classique <br>que nous avons soigneusement affiné au rhum pendant un an. <br>Il est exceptionnellement parfumé et combien savoureux! <br>Conservez-le au frais et servez-le à la température ambiante, <br>accompagné d'un porto ou d'un cidre de glace.<br>
+            <br>Disponible en grand format.
+        </p>
+        <img class="product-image" src="img/distingueunan1.jpg" alt="distingueunan1" width="890" height="490" />
+    </div>
+</article>
