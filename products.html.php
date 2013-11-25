b48b01f3b4c64e7bb3490eb1ea888521b62ead50
Pascal Gagneur <pascal.gagneur@postkodlotteriet.se>
2013-11-25 16:40:25 +0100

    moved products styling to inline style for easy editing

--- products.html.php
+++ products.html.php
@@ -1,6 +1,6 @@
 <h1><span>Produits</span></h1>
-<article id="prod5">
-    <h2>Product name</h2>
+<article id="prod5" style="background-color: #9c6720;">
+    <h2 style="background-image: url(../img/memorable_header.png);">Product name</h2>
     <div class="product-info">
         <p class="product-text">
         Voici Le Mémorable, notre gâteau aux fruits au sirop d'érable. <br>Il est rempli de fruits et de noix, et affiné au whisky pendant 30 jours. <br>
@@ -10,8 +10,8 @@
       <img class="product-image" src="img/memorable.png" alt="memorable" width="890" height="490" />
     </div>
 </article>
-<article id="prod1">
-    <h2>Product name</h2>
+<article id="prod1" style="background-color: #59371f;">
+    <h2 style="background-image: url(../img/le_distingue_header.png);">Product name</h2>
     <div class="product-info">
     <p class="product-text">
         Voici Le Distingué, notre gâteau aux fruits classique. <br>Il est rempli de fruits et de noix, et affiné au rhum pendant 30 jours. <br>Pour profiter pleinement de sa saveur, conservez-le au frais et servez-le <br>à la température ambiante. Il honorera les meilleurs portos!<br>
@@ -22,8 +22,8 @@
     <img class="product-image" src="img/distingue3.jpg" alt="distingue3" width="890" height="490" />
     </div>
 </article>
-<article id="prod6">
-    <h2>Product name</h2>
+<article id="prod6" style="background-color: #43291b;">
+    <h2 style="background-image: url(../img/audacieux_header.png);">Product name</h2>
     <div class="product-info">
         <p class="product-text">
         Voici L'Audacieux, notre gâteau aux fruits au chocolat noir à 70% de cacao. <br>Il est rempli de fruits, de noix, et affiné à la bière noire pendant 30 jours. <br>
@@ -33,8 +33,8 @@
       <img class="product-image" src="img/audacieux.png" alt="audacieux" width="890" height="490" />
     </div>
 </article>
-<article id="prod4">
-    <h2>Product name</h2>
+<article id="prod4" style="background-color: #2e2110;">
+    <h2 style="background-image: url(../img/distingue_header.png);">Product name</h2>
     <div class="product-info">
         <p class="product-text">
         Voici Le Distingué - Édition un an, notre gâteau aux fruits classique <br>que nous avons soigneusement affiné au rhum pendant un an. <br>Il est exceptionnellement parfumé et combien savoureux! <br>Conservez-le au frais et servez-le à la température ambiante, <br>accompagné d'un porto ou d'un cidre de glace.<br>
@@ -43,8 +43,8 @@
       <img class="product-image" src="img/distingueunan1.jpg" alt="distingueunan1" width="890" height="490" />
     </div>
 </article>
-<article id="prod2">
-    <h2>Product name</h2>
+<article id="prod2" style="background-color: #d2003c;">
+    <h2 style="background-image: url(../img/pretentieux_header.png);">Product name</h2>
     <div class="product-info">
         <p class="product-text">
             Voici Le Distingué - Édition un an, notre gâteau aux fruits classique <br>que nous avons soigneusement affiné au rhum pendant un an. <br>Il est exceptionnellement parfumé et combien savoureux! <br>Conservez-le au frais et servez-le à la température ambiante, <br>accompagné d'un porto ou d'un cidre de glace.<br>
